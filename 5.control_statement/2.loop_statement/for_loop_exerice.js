a3b96c9f23ca509c9449f5d70219d3487c14efe9
Nahidul Islam <analyticalnahid@gmail.com>
2023-09-24 18:22:56 +0600

    Create for_loop_exerice.js

---
+++ 5.control_statement/2.loop_statement/for_loop_exerice.js
@@ -0,0 +1,11 @@
+// File: multiplication_table.js
+
+function generateMultiplicationTable(n) {
+  for (let i = 1; i <= 10; i++) {
+    console.log(`${n} * ${i} = ${n * i}`);
+  }
+}
+
+// Generate a multiplication table for 5
+generateMultiplicationTable(5);
+
